4f43c097c70389be60471eacc2eef381eccca9c9
Alison8547 <alisonailson100@hotmail.com>
2024-02-19 22:34:54 -0300

    secao6 e exercicios

---
+++ src/main/java/secao6/For.java
@@ -0,0 +1,31 @@
+package secao6;
+
+public class For {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        print();
+
+        for (int i = 0; i < 20; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+        print();
+
+        for (int i = 99; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        print();
+
+        for (int i = 0; i < 20; i += 2) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void print() {
+        System.out.println("----------");
+    }
+}
