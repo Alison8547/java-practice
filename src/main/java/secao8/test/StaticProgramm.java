078476f9af0ca2dee8d154a5f9001f320a86e13e
Alison8547 <alisonailson100@hotmail.com>
2024-03-06 22:22:47 -0300

    static variavel e metodo

---
+++ src/main/java/secao8/test/StaticProgramm.java
@@ -0,0 +1,22 @@
+package secao8.test;
+
+import java.util.Scanner;
+
+public class StaticProgramm {
+    public static final double PI = 3.14159;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Entre com o radius:");
+        double radius = scanner.nextDouble();
+        double c = circumference(radius);
+
+        System.out.printf("Circumference: %.2f%n", c);
+
+    }
+
+    public static double circumference(double radius) {
+        return 2.0 * PI * radius;
+    }
+}
