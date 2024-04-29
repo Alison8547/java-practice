f527c128673c1d06926972d092a687d3a40b5ca7
Alison8547 <alisonailson100@hotmail.com>
2024-04-28 22:55:15 -0300

    exercicios vetores finalizados

--- src/main/java/secao10/test/ExerciciosVetores.java
+++ src/main/java/secao10/test/ExerciciosVetores.java
@@ -8,7 +8,7 @@ public class ExerciciosVetores {
     private static final Scanner scannerInt = new Scanner(System.in);
 
     public static void main(String[] args) {
-        ex8();
+        ex11();
     }
 
     public static void ex1() {
@@ -221,6 +221,106 @@ public class ExerciciosVetores {
         } else {
             System.out.println("NENHUM NUMERO PAR");
         }
+
+    }
+
+    public static void ex9() {
+        System.out.println("Quantos pessoas você vai digitar ?");
+        int n = scannerInt.nextInt();
+        String[] nomes = new String[n];
+        int[] idades = new int[n];
+        int minValue = Integer.MIN_VALUE;
+        String velha = "";
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.printf("Dados da %d pessoa:%n", ++count);
+
+            nomes[i] = scanner.nextLine();
+            idades[i] = scannerInt.nextInt();
+
+            if (idades[i] > minValue) {
+                minValue = idades[i];
+                velha = nomes[i];
+            }
+        }
+
+        System.out.println("PESSOA MAIS VELHA = " + velha);
+
+    }
+
+    public static void ex10() {
+        System.out.println("Quantos pessoas você vai digitar ?");
+        int n = scannerInt.nextInt();
+        String[] nomes = new String[n];
+        double[] nota1 = new double[n];
+        double[] nota2 = new double[n];
+        double[] sumNotas = new double[n];
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Digite nome, primeira e segunda nota do %d aluno:%n", ++count);
+            nomes[i] = scanner.nextLine();
+            nota1[i] = scannerDouble.nextDouble();
+            nota2[i] = scannerDouble.nextDouble();
+
+            sumNotas[i] = nota1[i] + nota2[i];
+        }
+
+        System.out.println("Alunos aprovados:");
+        for (int i = 0; i < n; i++) {
+            if (sumNotas[i] / 2 >= 6.0) {
+                System.out.println(nomes[i]);
+            }
+        }
+
+
+    }
+
+    public static void ex11() {
+        System.out.println("Quantos pessoas serão digitadas ?");
+        int n = scannerInt.nextInt();
+        double[] altura = new double[n];
+        String[] genero = new String[n];
+        int countHomens = 0;
+        int countMulheres = 0;
+        int count = 0;
+        int count2 = 0;
+        double mediaAlturaMulheres = 0.0;
+        double maxAltura = Double.MIN_VALUE;
+        double minAltura = Double.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Altura da %d pessoa: ", ++count);
+            altura[i] = scannerDouble.nextDouble();
+            System.out.printf("Genero da %d pessoa: ", ++count2);
+            genero[i] = scanner.nextLine().toUpperCase();
+
+            if (altura[i] > maxAltura) {
+                maxAltura = altura[i];
+            }
+
+            if (altura[i] < minAltura) {
+                minAltura = altura[i];
+            }
+
+            if (genero[i].equalsIgnoreCase("F")) {
+                mediaAlturaMulheres += altura[i];
+                countMulheres++;
+            }
+
+            if (genero[i].equalsIgnoreCase("M")) {
+                countHomens++;
+            }
+
+        }
+
+        System.out.printf("Menor altura = %.2f%n", minAltura);
+        System.out.printf("Maior altura = %.2f%n", maxAltura);
+        System.out.printf("Media das alturas das mulheres = %.2f%n", mediaAlturaMulheres / countMulheres);
+        System.out.printf("Numeros de homens = %d%n", countHomens);
+
 
     }
 }
