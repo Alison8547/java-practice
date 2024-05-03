f20fdb12dcd2a13b63e5b5498f2380b8da986245
Alison8547 <alisonailson100@hotmail.com>
2024-05-02 22:21:50 -0300

    exercicio persionato e boxing - unboxing - class wrappers

---
+++ src/main/java/secao10/domain/Rent.java
@@ -0,0 +1,49 @@
+package secao10.domain;
+
+public class Rent {
+    private String name;
+    private String email;
+    private Integer room;
+
+    public Rent(String name, String email, Integer room) {
+        this.name = name;
+        this.email = email;
+        this.room = room;
+    }
+
+    public Rent() {
+    }
+
+    @Override
+    public String toString() {
+        return "Rent{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", room=" + room +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getRoom() {
+        return room;
+    }
+
+    public void setRoom(Integer room) {
+        this.room = room;
+    }
+}
---
+++ src/main/java/secao10/test/BoxingAndUnboxing.java
@@ -0,0 +1,24 @@
+package secao10.test;
+
+public class BoxingAndUnboxing {
+    public static void main(String[] args) {
+        // tipos primitivos não aceita valores null
+        int x = 10;
+
+        // boxing
+        Object obj = x;
+
+        System.out.println(obj);
+
+        // unboxing
+        int y = (int) obj;
+
+        System.out.println(y);
+
+        // classes wrappers exemplo: aceita valores null
+        Double wrapper = null;
+        System.out.println(wrapper);
+
+
+    }
+}
