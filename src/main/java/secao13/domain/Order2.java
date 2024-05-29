ba5705eb02c751dfeab7f41c473c40d154be19d2
Alison8547 <alisonailson100@hotmail.com>
2024-05-28 22:34:32 -0300

    Order 2

---
+++ src/main/java/secao13/domain/Order2.java
@@ -0,0 +1,86 @@
+package secao13.domain;
+
+import secao13.enums.OrderStatus;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order2 {
+    private LocalDateTime moment;
+    private OrderStatus status;
+    private Client client;
+    private List<OrderItem> orderItems = new ArrayList<>();
+    private final DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    private final DateTimeFormatter dtf3 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public Order2(LocalDateTime moment, OrderStatus status, Client client) {
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+    }
+
+    public Order2() {
+    }
+
+    public void addItem(OrderItem item) {
+        orderItems.add(item);
+    }
+
+    public void removeItem(OrderItem item) {
+        orderItems.remove(item);
+    }
+
+    public Double total() {
+        Double total = 0.0;
+        for (OrderItem item : orderItems) {
+            total += item.subTotal();
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("ORDER SUMMARY:" + "\n");
+        sb.append("Order moment: ").append(moment.format(dtf2)).append("\n");
+        sb.append("Order status: ").append(status).append("\n");
+        sb.append("Client: ").append(client.getName()).append(" (").append(client.getBirthDate().format(dtf3)).append(")").append(" - ").append(client.getEmail()).append("\n");
+        sb.append("Order items:" + "\n");
+        for (OrderItem item : orderItems) {
+            sb.append(item.getProduct().getName()).append(", ").append("$").append(String.format("%.2f", item.getProduct().getPrice())).append(", ").append("Quantity: ").append(item.getQuantity()).append(", ").append("Subtotal: ").append("$").append(String.format("%.2f", item.subTotal())).append("\n");
+        }
+        sb.append("Total price: " + "$").append(String.format("%.2f", total())).append("\n");
+
+        return sb.toString();
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public LocalDateTime getMoment() {
+        return moment;
+    }
+
+    public void setMoment(LocalDateTime moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
---
+++ src/main/java/secao13/domain/OrderItem.java
@@ -0,0 +1,35 @@
+package secao13.domain;
+
+public class OrderItem {
+    private Integer quantity;
+    private Product product;
+
+    public OrderItem(Integer quantity, Product product) {
+        this.quantity = quantity;
+
+        this.product = product;
+    }
+
+    public OrderItem() {
+    }
+
+    public Double subTotal() {
+        return product.getPrice() * quantity;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
