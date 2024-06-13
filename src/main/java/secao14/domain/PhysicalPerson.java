a723972319e8c1f22bbd743018f448cdca87d4a1
Alison8547 <alisonailson100@hotmail.com>
2024-06-12 22:05:11 -0300

    tax person

---
+++ src/main/java/secao14/domain/PhysicalPerson.java
@@ -0,0 +1,38 @@
+package secao14.domain;
+
+public class PhysicalPerson extends Person {
+    private Double healthExpenses;
+
+    public PhysicalPerson(String name, Double annualIncome, Double healthExpenses) {
+        super(name, annualIncome);
+        this.healthExpenses = healthExpenses;
+    }
+
+    public PhysicalPerson() {
+        super();
+    }
+
+    @Override
+    public Double calculateTax() {
+        if (annualIncome < 2000.0 && healthExpenses > 0.0) {
+            return (annualIncome * 15) / 100.0 - (healthExpenses * 50) / 100.0;
+
+        } else if (annualIncome > 2000.0 && healthExpenses > 0.0) {
+            return (annualIncome * 25) / 100.0 - (healthExpenses * 50) / 100.0;
+
+        } else if (annualIncome < 2000.0) {
+            return (annualIncome * 15) / 100.0;
+        } else {
+            return (annualIncome * 25) / 100.0;
+        }
+
+    }
+
+    public Double getHealthExpenses() {
+        return healthExpenses;
+    }
+
+    public void setHealthExpenses(Double healthExpenses) {
+        this.healthExpenses = healthExpenses;
+    }
+}
