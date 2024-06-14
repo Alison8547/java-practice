0102eff6625efeff7b081e675a6acc14e2859996
Alison8547 <alisonailson100@hotmail.com>
2024-06-14 19:23:40 -0300

    stack trace

---
+++ src/main/java/secao15/test/StackTraceTest.java
@@ -0,0 +1,35 @@
+package secao15.test;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class StackTraceTest {
+    public static void main(String[] args) {
+        method1();
+    }
+
+    public static void method1() {
+        System.out.println("START METHOD 1");
+        method2();
+        System.out.println("END METHOD 1");
+    }
+
+    public static void method2() {
+        System.out.println("START METHOD 2");
+        try {
+            Scanner sc = new Scanner(System.in);
+            String[] names = sc.nextLine().split(" ");
+            int i = sc.nextInt();
+            System.out.println(names[i]);
+
+        } catch (InputMismatchException e) {
+            System.err.println("Imput error!");
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Position not found!");
+            e.printStackTrace();
+        }
+
+        System.out.println("End of program");
+        System.out.println("END METHOD 2");
+    }
+}
