64a623874668347ad2696b9bbc0415b8bb932f1c
Alison8547 <alisonailson100@hotmail.com>
2024-06-22 20:58:50 -0300

    TreeSet compareTo

---
+++ src/main/java/secao19/test/SetEqualTest2.java
@@ -0,0 +1,19 @@
+package secao19.test;
+
+import secao19.domain.Product;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetEqualTest2 {
+    public static void main(String[] args) {
+        Set<Product> set = new TreeSet<>();
+        set.add(new Product("TV", 1200.0));
+        set.add(new Product("PC", 4000.0));
+        set.add(new Product("Iphone", 3200.0));
+
+        for (Product p : set) {
+            System.out.println(p);
+        }
+    }
+}
