06a7b9890e99ca9aff9111efa46a028bb5784d7b
Alison8547 <alisonailson100@hotmail.com>
2024-06-27 15:21:25 -0300

    Comparator lambda

---
+++ src/main/java/secao20/domain/MyComparator.java
@@ -0,0 +1,14 @@
+package secao20.domain;
+
+import secao19.domain.Product;
+
+import java.util.Comparator;
+
+public class MyComparator implements Comparator<Product> {
+    @Override
+    public int compare(Product o1, Product o2) {
+        return o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase());
+    }
+
+
+}
---
+++ src/main/java/secao20/test/ComparatorTest.java
@@ -0,0 +1,27 @@
+package secao20.test;
+
+import secao19.domain.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComparatorTest {
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("TV", 6.0));
+        list.add(new Product("Iphone", 3200.0));
+        list.add(new Product("PC", 4000.0));
+        list.add(new Product("Alicate", 20.0));
+
+        //  list.sort(new MyComparator());
+
+//        Comparator<Product> comp = (p1, p2) -> p1.getPrice().compareTo(p2.getPrice());
+//        list.sort(comp);
+
+        list.sort((p1, p2) -> p1.getName().compareTo(p2.getName()));
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+    }
+}
