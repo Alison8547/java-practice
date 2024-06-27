912f96849b47423a0a1daf5be3d54f192349de47
Alison8547 <alisonailson100@hotmail.com>
2024-06-27 16:28:44 -0300

    ProductService method with predicate

---
+++ src/main/java/secao20/service/ProductService.java
@@ -0,0 +1,20 @@
+package secao20.service;
+
+import secao19.domain.Product;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ProductService {
+
+    public double filterSum(List<Product> products, Predicate<Product> criteria) {
+        double sum = 0.0;
+
+        for (Product product : products) {
+            if (criteria.test(product)) {
+                sum += product.getPrice();
+            }
+        }
+        return sum;
+    }
+}
