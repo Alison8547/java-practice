87bd02c6ff197336ea1a6134b741ac00a69557b4
Alison8547 <alisonailson100@hotmail.com>
2024-12-18 21:36:48 -0300

    partitioningBy method

---
+++ src/main/java/streams/test/Exemplos.java
@@ -0,0 +1,25 @@
+package streams.test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Exemplos {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        Map<Boolean, List<Integer>> collect = partitioning(numeros, n -> n > 5);
+
+        System.out.println(collect);
+
+    }
+
+    public static <T> Map<Boolean, List<T>> partitioning(List<T> list, Predicate<T> predicate) {
+        return list.stream()
+                .collect(Collectors.partitioningBy(predicate, Collectors.toList()));
+
+    }
+}
+
